21bee5818a2d9de3a97824548b4aa71581c6d07b
OmegaWiz <kraiwish123@gmail.com>
2026-05-05 18:03:29 +0700

    tab refactor

--- src/parse/t_opts.c
+++ src/parse/t_opts.c
@@ -16,38 +16,38 @@
  * returns 0 and set flag to true if match, returns 1 if not match
  */
 int toggle_opt(const char o) {
-  if (o == 'a')
-    g_opts.all = 1;
-  else if (o == 'l')
-    g_opts.long_format = 1;
-  else if (o == 'R')
-    g_opts.recursive = 1;
-  else if (o == 'r')
-    g_opts.reverse_order = 1;
-  else if (o == 't')
-    g_opts.time_sort = 1;
-  else
-    return (1);
-  return (0);
+	if (o == 'a')
+		g_opts.all = 1;
+	else if (o == 'l')
+		g_opts.long_format = 1;
+	else if (o == 'R')
+		g_opts.recursive = 1;
+	else if (o == 'r')
+		g_opts.reverse_order = 1;
+	else if (o == 't')
+		g_opts.time_sort = 1;
+	else
+		return (1);
+	return (0);
 }
 
 /**
  *
  */
 int render_flags(char *arg) {
-  size_t i;
+	size_t i;
 
-  i = 0;
-  if (arg[i++] != '-')
-    return (0);
-  if (arg[i] == '-') {
-    if (arg[++i] == '\0')
-      return (0);
-  } else {
-    while (arg[i]) {
-      if (!toggle_opt(arg[i++]))
-        return (2);
-    }
-  }
-  return (1);
+	i = 0;
+	if (arg[i++] != '-')
+		return (0);
+	if (arg[i] == '-') {
+		if (arg[++i] == '\0')
+			return (0);
+	} else {
+		while (arg[i]) {
+			if (!toggle_opt(arg[i++]))
+				return (2);
+		}
+	}
+	return (1);
 }
